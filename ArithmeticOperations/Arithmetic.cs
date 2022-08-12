f50c850d130eedc84c69e7cb15d2b7489665c1f1
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-12 22:56:56 +0530

    [Darshan]: Review on 11 Aug , Aritmetic operations

---
+++ ArithmeticOperations/Arithmetic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperations
+{
+    internal class Arithmetic
+    {
+
+        public void Add(float a,float b)
+        {
+            Console.WriteLine( "Addition of numbers is: "+ (a + b));
+
+        }
+
+        public void Subtract(float a, float b)
+        {
+            Console.WriteLine("Substraction of numbers is: " + (a - b));
+        }
+
+        public void Multiply(float a, float b)
+        {
+            Console.WriteLine("Multiplication of numbers is: " + (a * b));
+        }
+
+        public void Divide(float a, float b)
+        {
+            Console.WriteLine("Division of numbers(a/b) is: " + (a / b));
+        }
+    }
+}
---
+++ ArithmeticOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ArithmeticOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arithmetic operations!!!");
+
+            Console.WriteLine("Enter the two  numbers");
+            float firstNumber=float.Parse(Console.ReadLine());
+            float secondNumber= float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Substraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("select which Arithmetic operation  you want to perform ");
+            int ans=int.Parse(Console.ReadLine());
+
+
+            Arithmetic operation=new Arithmetic();
+
+            switch (ans) 
+            {
+                case 1: operation.Add(firstNumber,secondNumber);
+                    break;
+                
+                case 2: operation.Subtract(firstNumber, secondNumber);
+                    break;
+               
+                case 3: operation.Multiply(firstNumber, secondNumber);
+                    break;
+                
+                case 4: operation.Divide(firstNumber, secondNumber);
+                    break;
+                
+                default:Console.WriteLine("Enter valid selection");
+                    break;
+
+            }
+
+
+        }
+    }
+}
